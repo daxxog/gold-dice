26b786a5312f5fbaeb33c8d8159e69b1b3a17ae0
David Volm <david@volminator.com>
2016-01-19 05:54:23 +0000

    rolling!

--- cli.js
+++ cli.js
@@ -5,6 +5,24 @@
  * http://www.apache.org/licenses/LICENSE-2.0.html  
  */
 
-var GoldDice = require('./gold-dice.min.js');
+var GoldDice = require('./gold-dice.min.js'),
+    gd = new GoldDice();
 
-new GoldDice();
+console.log(gd.roll(2, 10000).length);
+
+console.log('testing 1k rolls of four sided dice. please be patient');
+console.log('average roll: ' + gd.roll(4, 1000).reduce(function(p, c) {
+    return parseInt(p, 10) + parseInt(c, 10);
+}) / 1000);
+
+
+console.log('testing 10k rolls of four sided dice. please be patient');
+console.log('average roll: ' + gd.roll(4, 10000).reduce(function(p, c) {
+    return parseInt(p, 10) + parseInt(c, 10);
+}) / 10000);
+
+
+console.log('testing 100k rolls of four sided dice. please be patient');
+console.log('average roll: ' + gd.roll(4, 100000).reduce(function(p, c) {
+    return parseInt(p, 10) + parseInt(c, 10);
+}) / 100000);
--- gold-dice.js
+++ gold-dice.js
@@ -20,7 +20,72 @@
         root.GoldDice = factory();
   }
 }(this, function() {
-    var GoldDice;
+    var GoldDice,
+        Crypto = require('bitcore-lib').crypto;
+    
+    GoldDice = function(constructor) {
+        if(constructor) { //todo add constructor / serialization
+        }
+        
+        if(!(Buffer.isBuffer(this.server) && (this.server.length === 64))) {
+            this.server = Crypto.Hash.sha512(new Buffer(Math.random().toString(), 'utf8'));
+        }
+        
+        if(!(Buffer.isBuffer(this.client) && (this.server.length === 32))) {
+            this.client = Crypto.Hash.sha256(new Buffer(Math.random().toString(), 'utf8'));
+        }
+        
+        if(typeof this.nonce !== 'number') {
+            this.nonce = 0;
+        }
+    };
+    
+    GoldDice.kalc = function(k, digits) {
+        return k.toString(10).split('').reverse().splice(0, digits).join('');
+    };
+    
+    GoldDice.k = function(s, o) {
+        return parseInt(s[0 + o] + s[1 + o] + s[2 + o] + s[3 + o] + s[4 + o], 16);
+    };
+    
+    GoldDice.UPPER_LIMIT = 1000000; //0xfffff - 1000000 = 48575
+    GoldDice.MAX_SEEK = 6; //Math.floor(b.length / 5)
+    
+    GoldDice.prototype.roll = function(digits, rolls) {
+        var result = [],
+            p, b, s, k, o, r;
+        
+        for(var i = 0; i < rolls; i++) {
+            r = false;
+            o = 10;
+            this.nonce += 1;
+            
+            p = Crypto.Hash.hmac(Crypto.Hash.sha256, new Buffer(this.nonce.toString(), 'utf8'), this.client);
+            b = Crypto.Hash.hmac(Crypto.Hash.sha256, p, this.server);
+            
+            s = b.toString('hex').split('');
+            k = GoldDice.k(s, o);
+            
+            for(var j = 0; j < GoldDice.MAX_SEEK; j++) {
+                if(k < GoldDice.UPPER_LIMIT) {
+                    r = GoldDice.kalc(k, digits);
+                    break;
+                } else {
+                    o += 5;
+                    k = GoldDice.k(s, o);
+                }
+            }
+            
+            if(r === false) { //re roll
+                i--;
+            } else {
+                result.push(r);
+            }
+            
+        }
+        
+        return result;
+    };
     
     return GoldDice;
 }));
